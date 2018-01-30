04e5d8d2e9a5b2feea68ae331c72a71a1e7ab24e
rainer.toots@khk.ee <7YJubYLY>
2018-01-30 11:06:27 +0200

    Defining variables and constants

--- praks1/valjastus.php
+++ praks1/valjastus.php
@@ -12,3 +12,10 @@
  */
 echo "Hello World!"."<br />"; // echo demonstratsioon
 print ("Hello Worley!<br />"); // print demonstratsioon
+// muutuja defineerimine
+$eesnimi = "Rainer"; // muutuja eesnimi = "Rainer" string
+$perenimi = "Toots"; // muutuja perenimi = "Toots" string
+$bussiNumber = 4; // muutuja bussiNumber = 4 integer
+$soiduKestvus = 0.8; // muutuja soiduKestvus = 0.8 float
+// konstandite defineerimine
+define("NUMBER_PI", 3.14); // constant NUMBER_PI väärtus on 3.14
