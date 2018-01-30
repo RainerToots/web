1525120be6913e6b7a14fa7dcebef228f72b9f61
rainer.toots@khk.ee <7YJubYLY>
2018-01-30 11:39:22 +0200

    Calculations for food at school's restaurant.

--- praks1/soogikalkulaator.php
+++ praks1/soogikalkulaator.php
@@ -6,3 +6,12 @@
  * Time: 11:28
  */
 
+$soogihind = 2.55; //soogihind einele
+$soodusprotsent = 15; // 15% protsenti
+$toetus = 1.8; // hetkeline toetus
+
+if ($soogihind > 0 and $soodusprotsent > 0 and $toetus > 0); {
+    $soodushind = $soogihind * ((100 - $soodusprotsent) / 100);
+    $hindop = $soodushind - $toetus;
+    echo "Teie söögihinnaks oleks " .$hindop."<br />";
+}
