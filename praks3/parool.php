72ae88bf0700fbb9d43364380f428e01a64bf181
rainer.toots@khk.ee <7YJubYLY>
2018-02-22 13:54:18 +0200

    Paroolid.

---
+++ praks3/parool.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rainer.toots
+ * Date: 22.02.2018
+ * Time: 13:22
+ */
+
+define('AB_HOST', 'localhost'); // DB host
+define('AB_USER', 'tootsraineriktkh'); // DB user
+define('AB_PASS', '_nJh.@vZGOk]'); // DB user password
+define('AB_NIMI', 'tootsrai_abhaldus'); // DB name
