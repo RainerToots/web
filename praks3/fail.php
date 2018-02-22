9adfe80be5f3cafbfed341e682a35b865f81f2cd
rainer.toots@khk.ee <7YJubYLY>
2018-02-22 13:13:37 +0200

    ajast info salvestamine fail.php abil

---
+++ praks3/fail.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rainer.toots
+ * Date: 22.02.2018
+ * Time: 13:07
+ */
+
+echo '<pre>';
+priunt_r($_POST);
+echo '</pre>';
+extract($_POST);
+
+echo $eesnimi'<br/>';
+echo $perenimi'<br/>';
+echo $aasta'<br/>';
+echo $kuu'<br/>';
+echo $paev'<br/>';
+
+$aeg = $aasta.'-'.$kuu.'-'.$paev;
+echo $aeg.'<br />';
